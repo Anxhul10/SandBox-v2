cbc62bbbb8bf0bd554396d775954339e6ddec32e
Anxhul10 <anxhul10@gmail.com>
2025-11-06 02:27:47 +0530

    chore: json read and write usign bufio

--- apps/task-tracker-cli/misc/Write.go
+++ apps/task-tracker-cli/misc/Write.go
@@ -1,20 +1,79 @@
 package misc
 
 import (
-	_"encoding/json"
-	_"fmt"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"bufio"
+	"io"
 )
 
+type Human struct {
+    Name string
+    Age int
+    Address string
+}
+
+// Source - https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
+// Posted by zouying
+// Retrieved 2025-11-06, License - CC BY-SA 4.0
+
+func readFileLines(logfile string) {
+    f, err := os.OpenFile(logfile, os.O_RDONLY, os.ModePerm)
+    if err != nil {
+        log.Fatalf("open file error: %v", err)
+        return
+    }
+    defer f.Close()
+
+    rd := bufio.NewReader(f)
+    for {
+        line, err := rd.ReadString('\n')
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+
+            log.Fatalf("read file line error: %v", err)
+            return
+        }
+        _ = line  // GET the line string
+		fmt.Print(line)
+    }
+}
+
+
 func ReadWrite() {
+	human1 := Human{Name:"Ankit", Age: 23, Address:"New Delhi"} 
+       
+    // encoding human1 struct 
+    // into json format 
+    human_enc, err := json.Marshal(human1) 
+       
+    if err != nil { 
+           
+        // if error is not nil 
+        // print error 
+        fmt.Println(err) 
+    } 
+       
+    // as human_enc is in a byte array 
+    // format, it needs to be  
+    // converted into a string  
+    fmt.Println(string(human_enc))
+
 	f, err := os.OpenFile("tasks.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
-        log.Fatal(err)
+		_, writeError := f.Write([]byte("[]"))
+        fmt.Print(writeError)
+		fmt.Print(err)
     }
-    if _, err := f.Write([]byte("data")); err != nil {
-        log.Fatal(err)
-    }
+
+	readFileLines("tasks.json")
+    // if _, err := f.Write([]byte("data")); err != nil {
+    //     log.Fatal(err)
+    // }
     if err := f.Close(); err != nil {
         log.Fatal(err)
     }
